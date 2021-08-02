846f29b6781011b5d5a94b6838f2c3def0b5a87e
hsicen <codinghuang@163.com>
2021-08-02 14:42:27 +0800

    天机触摸反馈设置入口

--- ruler/src/main/java/hsicen/ruler/KgNumberLayout.kt
+++ ruler/src/main/java/hsicen/ruler/KgNumberLayout.kt
@@ -17,95 +17,106 @@ import hsicen.ruler.databinding.LayoutKgNumberBinding
  * 描述：用于包着显示具体数字刻度Layout
  */
 class KgNumberLayout @JvmOverloads constructor(
-  context: Context,
-  attrs: AttributeSet? = null,
-  defStyleAttr: Int = 0
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr), RulerCallback {
-  private lateinit var binding: LayoutKgNumberBinding
+    private lateinit var binding: LayoutKgNumberBinding
 
-  //字体大小
-  private var mScaleTextSize = 12f.sp2px
-  private var mKgTextSize = 12f.sp2px
+    //字体大小
+    private var mScaleTextSize = 12f.sp2px
+    private var mKgTextSize = 12f.sp2px
 
-  //字体颜色
-  private var mScaleTextColor = color(R.color.yellow)
-  private var mKgTextColor = color(R.color.yellow)
-  private var scaleSpecail = false
+    //字体颜色
+    private var mScaleTextColor = color(R.color.yellow)
+    private var mKgTextColor = color(R.color.yellow)
+    private var scaleSpecail = false
 
-  //kg单位文字
-  private var mUnitText = "kg"
-  private var mRuler: BooheeRuler? = null
-  private val mCallbacks = ArrayList<RulerCallback>()
-  private var mLastScale = -1f
+    //kg单位文字
+    private var mUnitText = "kg"
+    private var mRuler: BooheeRuler? = null
+    private val mCallbacks = ArrayList<RulerCallback>()
+    private var mLastScale = -1f
 
-  init {
-    attrs?.let {
-      val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.KgNumberLayout, 0, 0)
-      mScaleTextSize = typedArray.getDimension(R.styleable.KgNumberLayout_scaleTextSize, mScaleTextSize)
-      mKgTextSize = typedArray.getDimension(R.styleable.KgNumberLayout_kgTextSize, mKgTextSize)
-      mScaleTextColor = typedArray.getColor(R.styleable.KgNumberLayout_scaleTextColor, mScaleTextColor)
-      mKgTextColor = typedArray.getColor(R.styleable.KgNumberLayout_kgTextColor, mKgTextColor)
-      mUnitText = typedArray.getString(R.styleable.KgNumberLayout_kgUnitText) ?: mUnitText
-      scaleSpecail = typedArray.getBoolean(R.styleable.KgNumberLayout_scaleSpecial, false)
+    // feedback
+    private var mHapticEnable = false
+    private var mHapFlag = 5
 
-      typedArray.recycle()
-    }
-    init(context)
-  }
+    init {
+        attrs?.let {
+            val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.KgNumberLayout, 0, 0)
+            mScaleTextSize = typedArray.getDimension(R.styleable.KgNumberLayout_scaleTextSize, mScaleTextSize)
+            mKgTextSize = typedArray.getDimension(R.styleable.KgNumberLayout_kgTextSize, mKgTextSize)
+            mScaleTextColor = typedArray.getColor(R.styleable.KgNumberLayout_scaleTextColor, mScaleTextColor)
+            mKgTextColor = typedArray.getColor(R.styleable.KgNumberLayout_kgTextColor, mKgTextColor)
+            mUnitText = typedArray.getString(R.styleable.KgNumberLayout_kgUnitText) ?: mUnitText
+            scaleSpecail = typedArray.getBoolean(R.styleable.KgNumberLayout_scaleSpecial, false)
 
-  private fun init(context: Context) {
-    val rootView = LayoutInflater.from(context).inflate(R.layout.layout_kg_number, this)
-    binding = LayoutKgNumberBinding.bind(rootView)
-
-    binding.tvScale.setTextSize(TypedValue.COMPLEX_UNIT_PX, mScaleTextSize)
-    binding.tvScale.setTextColor(mScaleTextColor)
-    binding.tvKg.setTextSize(TypedValue.COMPLEX_UNIT_PX, mKgTextSize)
-    binding.tvKg.setTextColor(mKgTextColor)
-    binding.tvKg.text = mUnitText
-  }
-
-  override fun onScaleChanging(scale: Float) {
-    mRuler?.let {
-      val tmp = if (scaleSpecail) RulerStringUtil.formatSpecial(scale, it.factor) else RulerStringUtil.formatValue(scale, it.factor)
-      if (tmp != binding.tvScale.text.toString()) {
-        binding.tvScale.text = tmp
-        mCallbacks.forEach { callback ->
-          performHapticFeedback(5)
-          callback.onScaleChanging(tmp.toFloat())
+            typedArray.recycle()
         }
-      }
+        init(context)
     }
-  }
 
-  override fun afterScaleChanged(scale: Float) {
-    val tmp = binding.tvScale.text.toString().toFloat()
-    if (tmp != mLastScale) {
-      mLastScale = tmp
-      mCallbacks.forEach { callback ->
-        callback.afterScaleChanged(mLastScale)
-      }
+    private fun init(context: Context) {
+        val rootView = LayoutInflater.from(context).inflate(R.layout.layout_kg_number, this)
+        binding = LayoutKgNumberBinding.bind(rootView)
+
+        binding.tvScale.setTextSize(TypedValue.COMPLEX_UNIT_PX, mScaleTextSize)
+        binding.tvScale.setTextColor(mScaleTextColor)
+        binding.tvKg.setTextSize(TypedValue.COMPLEX_UNIT_PX, mKgTextSize)
+        binding.tvKg.setTextColor(mKgTextColor)
+        binding.tvKg.text = mUnitText
     }
-  }
 
-  fun bindRuler(booheeRuler: BooheeRuler) {
-    mRuler = booheeRuler
-    booheeRuler.setCallback(this)
-  }
+    override fun onScaleChanging(scale: Float) {
+        mRuler?.let {
+            val tmp = if (scaleSpecail) RulerStringUtil.formatSpecial(scale, it.factor) else RulerStringUtil.formatValue(scale, it.factor)
+            if (tmp != binding.tvScale.text.toString()) {
+                binding.tvScale.text = tmp
+                mCallbacks.forEach { callback ->
+                    if (mHapticEnable) {
+                        performHapticFeedback(mHapFlag)
+                    }
+                    callback.onScaleChanging(tmp.toFloat())
+                }
+            }
+        }
+    }
 
-  fun configValue(config: TextView.() -> Unit) {
-    binding.tvScale.also(config)
-  }
+    override fun afterScaleChanged(scale: Float) {
+        val tmp = binding.tvScale.text.toString().toFloat()
+        if (tmp != mLastScale) {
+            mLastScale = tmp
+            mCallbacks.forEach { callback ->
+                callback.afterScaleChanged(mLastScale)
+            }
+        }
+    }
 
-  fun configUnit(config: TextView.() -> Unit) {
-    binding.tvKg.also(config)
-  }
+    fun bindRuler(booheeRuler: BooheeRuler) {
+        mRuler = booheeRuler
+        booheeRuler.setCallback(this)
+    }
 
-  fun addCallback(callback: RulerCallback) {
-    mCallbacks.add(callback)
-  }
+    fun configValue(config: TextView.() -> Unit) {
+        binding.tvScale.also(config)
+    }
 
-  fun removeCallback(callback: RulerCallback) {
-    mCallbacks.remove(callback)
-  }
+    fun configUnit(config: TextView.() -> Unit) {
+        binding.tvKg.also(config)
+    }
+
+    fun addCallback(callback: RulerCallback) {
+        mCallbacks.add(callback)
+    }
+
+    fun removeCallback(callback: RulerCallback) {
+        mCallbacks.remove(callback)
+    }
+
+    fun enableHaptic(enable: Boolean, flag: Int) {
+        mHapticEnable = enable
+        mHapFlag = flag
+    }
 
 }
